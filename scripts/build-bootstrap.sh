a4dfa8430f4a9d9edcc9d15a630b2a84ee781583
Simba Zhang <solderzzc@gmail.com>
2019-03-29 18:13:44 -0700

    To change base dir to /data/data/org.sharpai/files/usr

--- scripts/build-bootstrap.sh
+++ scripts/build-bootstrap.sh
@@ -21,7 +21,7 @@ BOOTSTRAP_ZIP=$TOPDIR/bootstraps/bootstrap-$TERMUX_ARCH.zip
 
 export TERMUX_TOPDIR TERMUX_ARCH TERMUX_DEBUG
 
-rm -rf $TERMUX_TOPDIR /gnu/usr
+rm -rf $TERMUX_TOPDIR /data/data/org.sharpai/files/usr
 
 $BUILDSCRIPT apt
 
@@ -32,7 +32,7 @@ printf "#!/system/bin/sh\nmklink(){\n   rm -f \$2\n   ln -s \$1 \$2\n}\n\n" > $S
 #echo $symlinks
 for item in $symlinks; do
 target=`readlink $item`
-[ "$target" == "/bin/sh" ] && target="/gnu4droid/usr/bin/applets/sh"
+[ "$target" == "/bin/sh" ] && target="/data/data/org.sharpai/files/usr/bin/applets/sh"
 echo "mklink $target $item" >> $SECOND_STAGE_SCRIPT
 unset target
 done
@@ -40,35 +40,35 @@ chmod +x $SECOND_STAGE_SCRIPT
 
 controls=`find ~/.termux-build -name "control" | grep massage/DEBIAN/control | grep -v subpackages`
 
-rm -f /gnu/usr/var/lib/dpkg/status
-touch /gnu/usr/var/lib/dpkg/status
+rm -f /data/data/org.sharpai/files/usr/var/lib/dpkg/status
+touch /data/data/org.sharpai/files/usr/var/lib/dpkg/status
 
-write_control_to_status() {	
-	grep "Architecture:" $1 >> /gnu/usr/var/lib/dpkg/status
-	if [ "$(grep "Depends:" $1)" != "" ]; then grep "Depends:" $1 >> /gnu/usr/var/lib/dpkg/status; fi
-	grep "Description:" $1 >> /gnu/usr/var/lib/dpkg/status
-	grep "Homepage:" $1 >> /gnu/usr/var/lib/dpkg/status
-	grep "Installed-Size:" $1 >> /gnu/usr/var/lib/dpkg/status
-	grep "Maintainer:" $1 >> /gnu/usr/var/lib/dpkg/status
-	grep "Package:" $1 >> /gnu/usr/var/lib/dpkg/status
-	grep "Version:" $1 >> /gnu/usr/var/lib/dpkg/status
-	echo "Status: install ok installed" >> /gnu/usr/var/lib/dpkg/status
-	echo >> /gnu/usr/var/lib/dpkg/status
+write_control_to_status() {
+	grep "Architecture:" $1 >> /data/data/org.sharpai/files/usr/var/lib/dpkg/status
+	if [ "$(grep "Depends:" $1)" != "" ]; then grep "Depends:" $1 >> /data/data/org.sharpai/files/usr/var/lib/dpkg/status; fi
+	grep "Description:" $1 >> /data/data/org.sharpai/files/usr/var/lib/dpkg/status
+	grep "Homepage:" $1 >> /data/data/org.sharpai/files/usr/var/lib/dpkg/status
+	grep "Installed-Size:" $1 >> /data/data/org.sharpai/files/usr/var/lib/dpkg/status
+	grep "Maintainer:" $1 >> /data/data/org.sharpai/files/usr/var/lib/dpkg/status
+	grep "Package:" $1 >> /data/data/org.sharpai/files/usr/var/lib/dpkg/status
+	grep "Version:" $1 >> /data/data/org.sharpai/files/usr/var/lib/dpkg/status
+	echo "Status: install ok installed" >> /data/data/org.sharpai/files/usr/var/lib/dpkg/status
+	echo >> /data/data/org.sharpai/files/usr/var/lib/dpkg/status
 }
 
 for item in $controls; do
-	write_control_to_status $item >> /gnu/usr/var/lib/dpkg/status
+	write_control_to_status $item >> /data/data/org.sharpai/files/usr/var/lib/dpkg/status
 done
 
-echo 
+echo
 
 target_dirs=`find $TERMUX_TOPDIR -name gnu | grep "/massage/gnu" | grep -v subpackages`
 for item in $target_dirs; do
 	package=$(basename $(dirname $(dirname $item)))
 	files=`find $item`
-	list_file="/gnu/usr/var/lib/dpkg/info/$package.list"
-	md5sums_file="/gnu/usr/var/lib/dpkg/info/$package.md5sums"
-	
+	list_file="/data/data/org.sharpai/files/usr/var/lib/dpkg/info/$package.list"
+	md5sums_file="/data/data/org.sharpai/files/usr/var/lib/dpkg/info/$package.md5sums"
+
 	rm -f $list_file $md5sums_file
 	echo >> $SECOND_STAGE_SCRIPT
 	echo "touch $md5sums_file" >> $SECOND_STAGE_SCRIPT
@@ -80,11 +80,10 @@ for item in $target_dirs; do
 	echo >> $SECOND_STAGE_SCRIPT
 done
 
-cp $SECOND_STAGE_SCRIPT /gnu/usr/bin/
+cp $SECOND_STAGE_SCRIPT /data/data/org.sharpai/files/usr/bin/
 
 mkdir -p `dirname $BOOTSTRAP_ZIP`
 rm -f $BOOTSTRAP_ZIP
-zip -yr $BOOTSTRAP_ZIP /gnu/*
+zip -yr $BOOTSTRAP_ZIP /data/data/org.sharpai/files/usr/*
 
 echo "Finished"
-
